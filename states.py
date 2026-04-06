d1586103831ae71cb0fca67817750bbaa18491cd
evarkhangelsky-stack <evarkhangelsky@gmail.com>
2026-04-06 18:32:19 -0300

    Update states.py

--- states.py
+++ states.py
@@ -3,3 +3,6 @@ from aiogram.fsm.state import State, StatesGroup
 class OrderState(StatesGroup):
     waiting_for_pickup_station = State()
     waiting_for_payment_method = State()
+
+class GalleryState(StatesGroup):
+    viewing = State()
